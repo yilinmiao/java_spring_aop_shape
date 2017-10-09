9ad157704f6e55715c786895fd080c387a90ba34
yilinmiao <yi.matheson@gmail.com>
2017-10-09 00:08:53 -0700

    Init java spring

---
+++ src/main/java/com/yilinmiao/AopMain.java
@@ -0,0 +1,16 @@
+package com.yilinmiao;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yilinmiao.service.ShapeService;
+
+public class AopMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+		System.out.println(shapeService.getCircle().getName());
+	}
+
+}
---
+++ src/main/java/com/yilinmiao/service/ShapeService.java
@@ -0,0 +1,21 @@
+package com.yilinmiao.service;
+
+import com.yilinmiao.model.Circle;
+import com.yilinmiao.model.Triangle;
+
+public class ShapeService {
+	private Circle circle;
+	private Triangle triangle;
+	public Circle getCircle() {
+		return circle;
+	}
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	public Triangle getTriangle() {
+		return triangle;
+	}
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+}
